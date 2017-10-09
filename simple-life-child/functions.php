20ca178060a7acf0cef8249fb291ab4e1692874d
maartenkuijpers <maarten@grasperk.nl>
2017-10-10 01:33:57 +0200

    Added collapse / expand option for Q&A section. Updated styling of block quote.

--- simple-life-child/functions.php
+++ simple-life-child/functions.php
@@ -7,6 +7,11 @@
 
 add_action( 'wp_enqueue_scripts', 'theme_enqueue_styles' );
 function theme_enqueue_styles() {
+    wp_register_script( 'jScript', 'https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js' );
+    wp_register_script('bootstrapjs',get_stylesheet_directory_uri() .'/js/bootstrap.min.js', array(), '3.3.7', true);
+    wp_enqueue_script( 'jScript' );
+    wp_enqueue_script( 'bootstrapjs' );
+
     wp_enqueue_style( 'parent-style', get_template_directory_uri() . '/style.css' );
     wp_enqueue_style( 'child-style', get_stylesheet_uri(), array( 'parent-style' ) );
 }
